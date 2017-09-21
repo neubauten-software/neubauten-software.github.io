6477a22a0d04bfbe988a6fa0011791666e0e318a
sergBull <bullsergv@gmail.com>
2017-09-22 00:30:23 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var posts = "channel";
 var xhr = new XMLHttpRequest();
 
 function Load() {
-  xhr.open("GET", "channel/admin/posts.xml");
+  xhr.open("GET", "channel/admin/posts.xml", true);
   xhr.send();
   
   xhr.onreadystatecange = function() {
