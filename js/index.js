eba0583e15c2a48f80c1c8e76c0c75673185f985
sergBull <bullsergv@gmail.com>
2017-09-22 00:12:16 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
 var posts = "channel";
 var xhr = new XMLHttpRequest();
+
+function Load() {
+  xhr.oppen("GET", "channel/admin/posts.xml");
+  xhr.send();
+  
+  xhr.onreadystatecange = function() {
+    alert(xhr.responseText);
+  });
+}
