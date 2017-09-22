b1bd6e5920229f3199f9ae80b76b39ee95ccb3d8
sergBull <bullsergv@gmail.com>
2017-09-22 15:37:29 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function LoadPosts(xml) {
-  var card = document.createElement("div");
+  var card = "<div class=\"card-view\">";
   var doc = xml.responseXML();
   
   card.className = "card-view";
@@ -23,13 +23,14 @@ function LoadPosts(xml) {
   var post = doc.getElementsByTagName("post");
   
   for (var i = 0; i < post.length; i++) {
-    card.innerHTML = "<img src\""
-      + post[i].getElementsByTagName("cover")[0].childNode[0].nodeValue
+    card.innerHTML += "<img src\""
+      + post[i].getElementsByTagName("cover")[0].childNodes[0].nodeValue
       + "\"/><br/><h1>" + post[i].getElementsByTagName("title")[0].childNodes[0].nodeValue
       + "</h1><p>" + post[i].getElementsByTagName("description")[0].childNodes[0].nodeValue
       + "</p><a href=\"" + post[i].getElementsByTagName("link")[0].childNodes[0].getAttribute("to")
       + "\">" + post[i].getElementsByTagName("link")[0].childNodes[0].getAttribute("title") + "</a>";
-    
-    document.getElementById("content").appendChild(card);
   }
+  
+  card.innerHTML += "</div>";
+  document.getElementById("content").innerHTML += card;
 }
