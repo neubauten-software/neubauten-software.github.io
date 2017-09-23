33b9566a225624546d3f86f065e256b0635365fa
sergBull <bullsergv@gmail.com>
2017-09-23 13:12:44 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,7 +5,22 @@ var xhr = new XMLHttpRequest();
 document.addEventListener("DOMContentLoaded", function () {
   xhr.onload = function() {
     if (this.readyState == 4 && this.status == 200) {
-      LoadPosts(this);
+      var card = "<div class=\"card-view\">";
+      var doc = this.responseXML;
+      var post = doc.getElementsByTagName("post");
+
+      for (var i = 0; i < post.length; i++) {
+        card += "<img src\""
+          + post[i].getElementsByTagName("cover")[0].childNodes[0].nodeValue
+          + "\"/><br/><h1>" + post[i].getElementsByTagName("title")[0].childNodes[0].nodeValue
+          + "</h1><p>" + post[i].getElementsByTagName("description")[0].childNodes[0].nodeValue
+          + "</p><a href=\"" + post[i].getElementsByTagName("link")[0].childNodes[0].nodeValue
+          + "\">" + post[i].getElementsByTagName("link")[0].childNodes[0].getAttribute("title") + "</a>";
+      }
+
+      card += "</div>";
+
+      alert(card);
     }
   };
   
@@ -15,20 +30,5 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function LoadPosts(xml) {
-  var card = "<div class=\"card-view\">";
-  var doc = xml.responseXML;
-  var post = doc.getElementsByTagName("post");
   
-  for (var i = 0; i < post.length; i++) {
-    card += "<img src\""
-      + post[i].getElementsByTagName("cover")[0].childNodes[0].nodeValue
-      + "\"/><br/><h1>" + post[i].getElementsByTagName("title")[0].childNodes[0].nodeValue
-      + "</h1><p>" + post[i].getElementsByTagName("description")[0].childNodes[0].nodeValue
-      + "</p><a href=\"" + post[i].getElementsByTagName("link")[0].childNodes[0].nodeValue
-      + "\">" + post[i].getElementsByTagName("link")[0].childNodes[0].getAttribute("title") + "</a>";
-  }
-  
-  card += "</div>";
-  
-  alert(card);
 }
