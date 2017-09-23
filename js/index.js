bcd824a5fecff772bc704a1eed4cd8731b0366a2
sergBull <bullsergv@gmail.com>
2017-09-23 23:43:38 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -32,30 +32,4 @@ document.addEventListener("DOMContentLoaded", function () {
   xhr.open("GET", "channel/admin/posts.xml");
   xhr.rersponseType = "document";
   xhr.send();
-  
-  xhr_2.onload = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var sidebar_content = "";
-      var doc = this.responseXML;
-      var logo = doc.getElementsByTagName("logo")[0],
-          cover = doc.getElementsByTagName("cover")[0],
-          name = doc.getElementsByTagName("name")[0],
-          id = doc.getElementsByTagName("id")[0],
-          description = doc.getElementsByTagName("description")[0],
-          vk_link = doc.getElementsByTagName("vk_link")[0];
-      
-      sidebar_content = "<img src=\"" + cover.childNodes[0].nodeValue + "\"/>"
-        + "<img src=\"" + logo.childNodes[0].nodeValue + "\"/>"
-        + "<h1>" + name.childNodes[0].nodeValue + "</h1>"
-        + "<h5>" + id.childNodes[0].nodeValue + "</h5>"
-        + "<p>" + description.childNodes[0].nodeValue + "</p>"
-        + "<a href=\"https://vk.com/" + vk_link.getAttribute("to") + "\">" + vk_link.getAttribute("title") + "</a>";
-      
-      document.getElementById("sidebar").innerHTML = sidebar_content;
-    }
-  };
-  
-  xhr_2.open("GET", "channel/admin/info.xml");
-  xhr_2.responseType = "document";
-  xhr_2.send();
 });
