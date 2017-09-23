d7155c46b9daaafac08a98ba1fe3d57b95b30770
sergBull <bullsergv@gmail.com>
2017-09-23 13:30:27 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,12 +10,18 @@ document.addEventListener("DOMContentLoaded", function () {
       var post = doc.getElementsByTagName("post");
 
       for (var i = 0; i < post.length; i++) {
-        card += "<div class=\"card-view\"><img src=\""
+        card += "<div class=\"card-view\">"
+          + "  <img src=\""
           + post[i].getElementsByTagName("cover")[0].childNodes[0].nodeValue
-          + "\"/><br/><h1>" + post[i].getElementsByTagName("title")[0].childNodes[0].nodeValue
-          + "</h1><p>" + post[i].getElementsByTagName("description")[0].childNodes[0].nodeValue
-          + "</p><a href=\"" + post[i].getElementsByTagName("link")[0].getAttribute("to")
-          + "\">" + post[i].getElementsByTagName("link")[0].getAttribute("title") + "</a></div>";
+          + "\"/><br/>"
+          + "  <h1>" + post[i].getElementsByTagName("title")[0].childNodes[0].nodeValue
+          + "</h1>"
+          + "  <p>" + post[i].getElementsByTagName("description")[0].childNodes[0].nodeValue
+          + "</p>"
+          + "  <a href=\"" + post[i].getElementsByTagName("link")[0].getAttribute("to")
+          + "\">" + post[i].getElementsByTagName("link")[0].getAttribute("title") + "</a>"
+          + "  <font color=\"#949494\">" + post[i].getElementsByTagName("id")[0].childNodes[0].nodeValue + "</font>"
+          + "</div>";
       }
 
       document.getElementById("content").innerHTML += (card);
