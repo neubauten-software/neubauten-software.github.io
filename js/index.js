edbdc20388ae7a0268b3d1a0c1000a6515d1240b
sergBull <bullsergv@gmail.com>
2017-09-23 13:32:01 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
           + "  <p>" + post[i].getElementsByTagName("description")[0].childNodes[0].nodeValue
           + "</p>"
           + "  <a href=\"" + post[i].getElementsByTagName("link")[0].getAttribute("to")
-          + "\">" + post[i].getElementsByTagName("link")[0].getAttribute("title") + "</a>"
+          + "\">" + post[i].getElementsByTagName("link")[0].getAttribute("title") + "</a><br/>"
           + "  <font color=\"#949494\">" + post[i].getElementsByTagName("id")[0].childNodes[0].nodeValue + "</font>"
           + "</div>";
       }
