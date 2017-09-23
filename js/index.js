25e6d343dc67daf85bda9207ec4d0690c7643102
sergBull <bullsergv@gmail.com>
2017-09-23 13:15:16 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,8 +14,8 @@ document.addEventListener("DOMContentLoaded", function () {
           + post[i].getElementsByTagName("cover")[0].childNodes[0].nodeValue
           + "\"/><br/><h1>" + post[i].getElementsByTagName("title")[0].childNodes[0].nodeValue
           + "</h1><p>" + post[i].getElementsByTagName("description")[0].childNodes[0].nodeValue
-          + "</p><a href=\"" + post[i].getElementsByTagName("link")[0].childNodes[0].nodeValue
-          + "\">" + post[i].getElementsByTagName("link")[0].childNodes[0].getAttribute("title") + "</a>";
+          + "</p><a href=\"" + /* post[i].getElementsByTagName("link")[0].childNodes[0].nodeValue */
+          + "\">" + /* post[i].getElementsByTagName("link")[0].childNodes[0].getAttribute("title")  */ + "</a>";
       }
 
       card += "</div>";
