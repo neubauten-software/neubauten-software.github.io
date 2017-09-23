f3f68f5f6797b3830c1f2abecf81da84f54fa897
sergBull <bullsergv@gmail.com>
2017-09-23 13:23:28 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
       var post = doc.getElementsByTagName("post");
 
       for (var i = 0; i < post.length; i++) {
-        card += "<div class=\"card-view\"><img src\""
+        card += "<div class=\"card-view\"><img src=\""
           + post[i].getElementsByTagName("cover")[0].childNodes[0].nodeValue
           + "\"/><br/><h1>" + post[i].getElementsByTagName("title")[0].childNodes[0].nodeValue
           + "</h1><p>" + post[i].getElementsByTagName("description")[0].childNodes[0].nodeValue
