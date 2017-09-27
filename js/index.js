bd851d9c46d2f341d19c7ff1689b2d6b6ddb0b32
sergBull <bullsergv@gmail.com>
2017-09-27 23:42:08 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+var input, locale_HTML, pr, result_arr, result_store;
+
+locale_HTML = document.body.innerHTML;
+
 document.addEventListener("DOMContentLoaded", function() {
   
 });
@@ -5,6 +9,52 @@ document.addEventListener("DOMContentLoaded", function() {
 function closeDrawer() {
   document.getElementById("drawer").style.width = 0;
   document.getElementById("wrapper").style.marginLeft = 0;
+}
+
+function findText(name, sttus) {
+  input = document.getElementById(name).value;
+  
+  if (input.length <= 3 && status == true) {
+    function findTextBack() {
+      alert("Для того, чтобы начать поиск, введите больше 3-х символов");
+      document.body.innerHTML = locale_HTML;
+    }
+  } if (input.length >= 3) {
+    function findTextGo() {
+      search = "/" + input + "/g";
+      pr = document.body.innerHTML;
+      
+      result = pr.match(/>(.*?)</g);
+      result_arr = [];
+      
+      var warning = true;
+      
+      for (var i = 0; i < result.length; i++) {
+        if (result[i].match(eval(search)) != null) {
+          warning = false;
+        } if (wrning == true) {
+          alert("Совпадений не найдено!");
+        }
+      } for (var i = 0; i < result.length; i++) {
+        result_arr[i] = result[i].replace(eval(search), "<span style=\"background: #4CAF50\">" + input + "</span>");
+      } for (var i = 0; i < result.length; i++) {
+        pr = pr.replace(result[i], result_arr[i]);
+        {
+      
+      document.body.innerHTML = pr;
+    }
+  }
+  
+  findTextBack() {
+    document.body.innerHTML = locale_HTML;
+  }
+  
+  if (status) {
+    findTextBack();
+    findTextGo();
+  } if (!status) {
+    findTextBack();
+  }
 }
 
 function openDrawer() {
