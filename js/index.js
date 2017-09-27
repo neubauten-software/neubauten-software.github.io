4400dd11f130f39e830ea396a80e941527be7755
sergBull <bullsergv@gmail.com>
2017-09-27 21:38:21 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-function openDrawer() {
-  document.getElementById("drawer").style.width = "250px";
-  document.getElementById("wrapper").style.marginLeft = "250px";
-}
+document.addEventListener("DOMContentLoaded", function() {
+  function openDrawer() {
+    document.getElementById("drawer").style.width = "250px";
+    document.getElementById("wrapper").style.marginLeft = "250px";
+  }
+});
