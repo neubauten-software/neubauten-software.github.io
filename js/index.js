84862045f2ef70d487f48e216b658310c620a732
sergBull <bullsergv@gmail.com>
2017-09-27 22:04:15 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
+  closeDrawer();
   openDrawer();
 });
+
+function closeDrawer() {
+  document.getElementById("drawer").style.width = 0;
+  document.getElementById("wrapper").style.marginLeft = 0;
+}
 
 function openDrawer() {
   document.getElementById("drawer").style.width = "250px";
