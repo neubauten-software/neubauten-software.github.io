32ee24bb7c591d589f3631ec80b10e6fb5d5d661
sergBull <bullsergv@gmail.com>
2017-09-28 20:36:44 +0300

    Add files via upload

---
+++ projects/connect.js
@@ -0,0 +1,27 @@
+/** Connect.js - Ð²ÐµÑ€ÑÐ¸Ñ 1.0_preview
+ *
+ *  @author        { Neubauten Software }
+ *  @version       { 1.0 }
+ *  
+ *  @constructor   { XMLParser }
+ *
+*/
+
+function XMLParser(Url, Method) {
+  var xhr = new XMLHttpRequest();
+  
+  xhr.onload = function() {
+    var doc = this.responseXML,
+        parseable = doc.getElementsByTagName("text");
+    
+    for (var i = 0; i < parseable.length; i++) {
+      console.log(parseable[i].childNodes.nodeValue);
+    }
+  };
+  
+  xhr.open(Method, Url, true);
+  xhr.responseType = "document";
+  xhr.send("");
+}
+
+XMLParser("my_first_xml.xml", "GET");
